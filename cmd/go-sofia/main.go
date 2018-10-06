9b3b8ccd6df357925610e5bd68f79dbce739629b
ialidzhikov <i.alidjikov@gmail.com>
2018-10-06 15:34:58 +0300

    Add Dockerfile

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -1,12 +1,24 @@
 package main
 
-import "log"
-import "fmt"
-import "net/http"
-import "os"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-import "github.com/gorilla/mux"
-import "github.com/ialidzhikov/go-sofia/internal/diagnostics"
+	"github.com/gorilla/mux"
+	"github.com/ialidzhikov/go-sofia/internal/diagnostics"
+)
+
+type serverConf struct {
+	port   string
+	router http.Handler
+	name   string
+}
 
 func main() {
 	log.Print("Starting the application...")
@@ -16,28 +28,72 @@ func main() {
 		log.Fatal("The application port should be set")
 	}
 
-	diagnosticsPort := os.Getenv("DIAG_PORT")
-	if len(diagnosticsPort) == 0 {
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
 		log.Fatal("The diagnostics port should be set")
 	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 
-	go func() {
-		error := http.ListenAndServe(":" + blPort, router)
-		if error != nil {
-			log.Fatal(error)
-		}
-	}()
-
 	diagnostics := diagnostics.NewDiagnostics()
-	err := http.ListenAndServe(":" + diagnosticsPort, diagnostics)
-	if err != nil {
-		log.Fatal(err)
+
+	possibleErrors := make(chan error, 2)
+
+	configurations := []serverConf{
+		{
+			port:   blPort,
+			router: router,
+			name:   "application server",
+		},
+		{
+
+			port:   diagPort,
+			router: diagnostics,
+			name:   "diagnostics server",
+		},
+	}
+
+	servers := make([]*http.Server, 2)
+
+	for i, c := range configurations {
+		go func(conf serverConf, i int) {
+			log.Printf("The %s is preparing to handle connections...", conf.name)
+			servers[i] = &http.Server{
+				Addr:    ":" + conf.port,
+				Handler: conf.router,
+			}
+			err := servers[i].ListenAndServe()
+			if err != nil {
+				possibleErrors <- err
+			}
+		}(c, i)
+	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-possibleErrors:
+		log.Printf("Got an error: %v", err)
+	case sig := <-interrupt:
+		log.Printf("Recevied the signal %v", sig)
+	}
+
+	for _, s := range servers {
+		timeout := 5 * time.Second
+		log.Printf("Shutdown with timeout: %s", timeout)
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		err := s.Shutdown(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+		log.Printf("Server gracefully stopped")
 	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	log.Print("The hello handler was called")
 	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
