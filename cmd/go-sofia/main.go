d47cb094c917a9b8a7a19dcc64e5770fb86363c3
ialidzhikov <i.alidjikov@gmail.com>
2018-10-06 12:12:43 +0300

    Make ports configurable

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -3,26 +3,36 @@ package main
 import "log"
 import "fmt"
 import "net/http"
+import "os"
+
 import "github.com/gorilla/mux"
 import "github.com/ialidzhikov/go-sofia/internal/diagnostics"
 
 func main() {
-	log.Print("Hello, world")
+	log.Print("Starting the application...")
+
+	blPort := os.Getenv("PORT")
+	if len(blPort) == 0 {
+		log.Fatal("The application port should be set")
+	}
+
+	diagnosticsPort := os.Getenv("DIAG_PORT")
+	if len(diagnosticsPort) == 0 {
+		log.Fatal("The diagnostics port should be set")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 
-	router.HandleFunc("/health")
-
 	go func() {
-		error := http.ListenAndServe(":8080", router)
+		error := http.ListenAndServe(":" + blPort, router)
 		if error != nil {
 			log.Fatal(error)
 		}
 	}()
 
 	diagnostics := diagnostics.NewDiagnostics()
-	err := http.ListenAndServe(":8585", diagnostics)
+	err := http.ListenAndServe(":" + diagnosticsPort, diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
