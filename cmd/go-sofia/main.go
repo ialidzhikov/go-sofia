8c03e459871bcb368a04948499ac801d52446113
ialidzhikov <i.alidjikov@gmail.com>
2018-10-06 11:42:02 +0300

    Add diagnostics

--- cmd/go-sofia/main.go
+++ cmd/go-sofia/main.go
@@ -1,14 +1,33 @@
 package main
 
 import "log"
+import "fmt"
 import "net/http"
+import "github.com/gorilla/mux"
+import "github.com/ialidzhikov/go-sofia/internal/diagnostics"
 
 func main() {
 	log.Print("Hello, world")
 
-	error := http.ListenAndServe(":8080", nil)
-	if error != nil {
-		log.Fatal(error)
-	}
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
 
+	router.HandleFunc("/health")
+
+	go func() {
+		error := http.ListenAndServe(":8080", router)
+		if error != nil {
+			log.Fatal(error)
+		}
+	}()
+
+	diagnostics := diagnostics.NewDiagnostics()
+	err := http.ListenAndServe(":8585", diagnostics)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
---
+++ internal/diagnostics/diagnostics.go
@@ -0,0 +1,19 @@
+package diagnostics
+
+import "github.com/gorilla/mux"
+
+func NewDiagnostics() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/healthz", healthz)
+	router.HandleFunc("/ready", ready)
+
+	return router
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
+
+func ready(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
